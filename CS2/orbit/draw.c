8399e521f1daea5fa7c0dc2a635042c5ebacefab
antwaves <184776428+antwaves@users.noreply.github.com>
2026-06-22 14:48:05 -0400

    Get a window appearing with glfw!

--- CS2/orbit/draw.c
+++ CS2/orbit/draw.c
@@ -1,34 +1,78 @@
-#include "include/GLFW/glfw3.h"
-#include "include/vulkan/vulkan.h"
-#include "include/cglm/cglm.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+struct App;
+void _init_window(struct App* self);
+void _init_vulkan(struct App* self);
+void _main_loop(struct App* self);
+void _clean_up(struct App* self);
+void run(struct App* self);
+
+
+struct App{
+    void (*run)(struct App* self);
+    GLFWwindow* window;
+    uint32_t WIDTH;
+    uint32_t HEIGHT;
+};
+
+
+void run(struct App* self) {
+    _init_window(self);
+    _init_vulkan(self);
+    _main_loop(self);
+    _clean_up(self);
+}
+
+
+void _init_window(struct App* self) {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    self->window = glfwCreateWindow(self->WIDTH, self->HEIGHT, "Vulkan", NULL, NULL);
+}
+
+
+void _init_vulkan(struct App* self) {
+
+}
+
+
+void _main_loop(struct App* self) {
+    while (!glfwWindowShouldClose(self->window)) {
+        glfwPollEvents();
+    }
+}
+
+
+void _clean_up(struct App* self) {
+    glfwDestroyWindow(self->window);
+    glfwTerminate();
+}
+
+
+struct App* init() {
+    struct App* a = malloc(sizeof(struct App));
+    a->run = run;
+    a->WIDTH = 800;
+    a->HEIGHT = 600;
+    return a;
+}
+
+
+void destroy_app(struct App* a) {
+    free(a);
+}
 
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", NULL, NULL);
-
-    uint32_t extension_count = 0;
-    vkEnumerateInstanceExtensionProperties(NULL, &extension_count, NULL);
-    printf("%u", extension_count);
-    mat4 matrix;
-    vec4 vec;
-    vec4 result;
-
-    glm_mat4_mul(matrix, &vec, &result);
-    
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
+    struct App* app = init();
+    app->run(app);
+    destroy_app(app);
 }
