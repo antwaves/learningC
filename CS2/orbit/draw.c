c2813eed3133a2aba3b378315bc68311eff7956c
antwaves <184776428+antwaves@users.noreply.github.com>
2026-06-23 12:00:25 -0400

    Add (and break) validation layers

--- CS2/orbit/draw.c
+++ CS2/orbit/draw.c
@@ -1,3 +1,4 @@
+#include "vulkan/vk_platform.h"
 #include "vulkan/vulkan_core.h"
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -18,24 +19,47 @@ const bool enable_validation_layers = true;
 
 // not debug stuff
 struct App;
+struct extension_info;
 void _init_window(struct App* self);
 void _init_vulkan(struct App* self);
 void _main_loop(struct App* self);
 void _clean_up(struct App* self);
-void check_extensions(const char** glfw_extensions, int glfw_extension_count, bool log);
-void check_validation_layers();
 void _create_instance(struct App* self);
+void _get_required_instance_extensions(struct App* self);
+void _setup_debug_messenger(struct App* self);
 void run(struct App* self);
 
-struct App{
+void check_extensions(const char** glfw_extensions, int glfw_extension_count, bool log);
+void check_validation_layers();
+
+struct extension_info {
+    uint32_t extension_count;
+    const char** extension_names;
+};
+
+
+struct App {
     uint32_t WIDTH;
     uint32_t HEIGHT;
     bool log;
 
     void (*run)(struct App* self);
+    struct extension_info extensions;
     GLFWwindow* window;
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debug_messenger;
 };
+
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT severity, \
+                                                     VkDebugUtilsMessageTypeFlagsEXT type, \
+                                                     const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data, \
+                                                     void* p_user_data) {
+    if (severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+        fprintf(stderr, "Validation Layer: type %u msg: %s\n", type, p_callback_data->pMessage);
+    }
+    return VK_FALSE;
+}
 
 
 void run(struct App* self) {
@@ -56,6 +80,7 @@ void _init_window(struct App* self) {
 
 void _init_vulkan(struct App* self) {
     _create_instance(self);
+    _setup_debug_messenger(self);
 }
 
 
@@ -70,10 +95,13 @@ void _clean_up(struct App* self) {
     vkDestroyInstance(self->instance, NULL);
     glfwDestroyWindow(self->window);
     glfwTerminate();
+    free(self->extensions.extension_names);
 }
 
 
 void _create_instance(struct App* self) {
+    check_validation_layers();
+
     VkApplicationInfo app_info = {0};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pApplicationName = "Hello Triangle";
@@ -85,16 +113,15 @@ void _create_instance(struct App* self) {
     VkInstanceCreateInfo create_info = {0};
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     create_info.pApplicationInfo = &app_info;
-    uint32_t glfw_extension_count = 0;
-    const char** glfw_extensions;
-    glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-    create_info.enabledExtensionCount = glfw_extension_count;
-    create_info.ppEnabledExtensionNames = glfw_extensions;
+    
+    _get_required_instance_extensions(self);
+    struct extension_info extension_info = self->extensions;
+    check_extensions(extension_info.extension_names, extension_info.extension_count, self->log);
+
+    create_info.enabledExtensionCount = extension_info.extension_count;
+    create_info.ppEnabledExtensionNames = extension_info.extension_names;
     create_info.enabledLayerCount = 0;
     create_info.ppEnabledLayerNames = NULL;
-
-    check_extensions(glfw_extensions, glfw_extension_count, self->log);
-    check_validation_layers();
     
     if (vkCreateInstance(&create_info, NULL, &(self->instance)) != VK_SUCCESS) {
         perror("Error: ");
@@ -103,12 +130,36 @@ void _create_instance(struct App* self) {
 }
 
 
-void check_extensions(const char** glfw_extensions, int glfw_extension_count, bool log) {
+void _get_required_instance_extensions(struct App* self) {
+    uint32_t glfw_extension_count = 0;
+    const char** glfw_extensions;
+    glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    if (!enable_validation_layers) {
+        self->extensions.extension_count = glfw_extension_count;        
+        self->extensions.extension_names = glfw_extensions;
+    }
+    else {
+        const char** all_extensions = calloc(1, sizeof(char*) * (glfw_extension_count + 1));
+        for (int i = 0; i < glfw_extension_count; i++) {
+            all_extensions[i] = glfw_extensions[i];
+        }
+        const char* validation = _strdup(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        all_extensions[glfw_extension_count] = validation;
+
+        self->extensions.extension_names = all_extensions;
+        self->extensions.extension_count = glfw_extension_count + 1;
+    }
+}
+
+
+void check_extensions(const char** glfw_extensions, int glfw_extension_count, bool log) { // TODO: PASS EXTENSIONS BACK OUT
     uint32_t extension_count = 0;
     vkEnumerateInstanceExtensionProperties(NULL, &extension_count, NULL);
 
     VkExtensionProperties* extensions = malloc(extension_count * sizeof(VkExtensionProperties));
     vkEnumerateInstanceExtensionProperties(NULL, &extension_count, extensions);
+
 
     if (log) {
         printf("%d extensions installed: \n", extension_count);
@@ -118,7 +169,7 @@ void check_extensions(const char** glfw_extensions, int glfw_extension_count, bo
     }
 
     if (log) {
-        printf("\n%d extensions needed for GLFW: \n", glfw_extension_count);
+        printf("\n%d extensions needed for GLFW%s: \n", glfw_extension_count, enable_validation_layers ? " and validation layers" : "");
     }
     bool missing_any = false;
     for (int i = 0; i < glfw_extension_count; i++) {
@@ -170,6 +221,27 @@ void check_validation_layers() {
 
     free(layers);
 }
+
+
+void _setup_debug_messenger(struct App* self) {
+    if (!enable_validation_layers) return;
+    VkDebugUtilsMessageSeverityFlagsEXT severity_flags = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | 
+                                                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT; 
+
+    VkDebugUtilsMessageTypeFlagsEXT message_type_flags = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | 
+                                                         VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | 
+                                                         VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT | 
+                                                         VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT;
+
+
+    VkDebugUtilsMessengerCreateInfoEXT debug_utils_messenger_create_info_ext = {0};
+    debug_utils_messenger_create_info_ext.messageSeverity = severity_flags;
+    debug_utils_messenger_create_info_ext.messageType = message_type_flags;
+    debug_utils_messenger_create_info_ext.pfnUserCallback = &debug_callback;
+
+    self->debug_messenger = self->instance->vkCreateDebugUtilsMessengerEXT()
+}
+
 
 struct App* init() {
     struct App* a = malloc(sizeof(struct App));
