58ee9be66e89453fe2bffc8844a347da098bc421
antwaves <184776428+antwaves@users.noreply.github.com>
2026-07-26 15:17:02 -0400

    Finish cartesian

--- cs3/cartesian/cartesian.c
+++ cs3/cartesian/cartesian.c
@@ -5,7 +5,8 @@
 
 
 char** product(char** a, char** b) {
-    int a_elms, b_elms = 0;
+    int a_elms = 0;
+    int b_elms = 0;
     char** p_a = a;
     char** p_b = b;
 
@@ -18,7 +19,7 @@ char** product(char** a, char** b) {
         p_b++;
     }
 
-    char** items = malloc(((a_elms * b_elms) + 1) * sizeof(char*));
+    char** prod = malloc(((a_elms * b_elms) + 1) * sizeof(char*));
     int index = 0;
     for (int i = 0; i < a_elms; i++) {
         for (int j = 0; j < b_elms; j++) {
@@ -26,22 +27,69 @@ char** product(char** a, char** b) {
             int b_length = strlen(b[j]);
             int internal_ind = 0;
             
-            items[index] = malloc((a_length + b_length + 2) * sizeof(char));
+            prod[index] = malloc((a_length + b_length + 2) * sizeof(char));
 
             for (int k = 0; k < a_length; k++) {
-                items[index][internal_ind++] = a[i][k];
+                prod[index][internal_ind++] = a[i][k];
             }
-            items[index][internal_ind++] = ' ';
+            prod[index][internal_ind++] = ' ';
             for (int k = 0; k < b_length; k++) {
-                items[index][internal_ind++] = b[j][k];
+                prod[index][internal_ind++] = b[j][k];
             }
-            items[index][internal_ind] = '\0';
+            prod[index][internal_ind] = '\0';
 
             index++;
         }
     }
-    items[index] = NULL;
-    return items;
+    prod[index] = NULL;
+    return prod;
+}
+
+
+char** multi_product(char*** arrs) {
+    char*** p_arr = arrs;
+    int elms = 0;
+    while (*p_arr != NULL) {
+        elms++;
+        p_arr++;
+    }
+
+    if (elms < 2) {
+        exit(1);
+    }
+    else if (elms == 2) {
+        return product(arrs[0], arrs[1]);
+    }
+    
+    char*** olds = malloc(sizeof(char**) * (elms - 2));
+    char** set = product(arrs[0], arrs[1]);
+    for (int i = 2; i < elms; i++) {
+        olds[i - 2] = set;
+        set = product(set, arrs[i]);
+    }
+    
+    for (int i = 0; i < elms - 2; i++) {
+        char** old_p = olds[i];
+        while (*old_p != NULL) {
+            free(*old_p);
+            old_p++;
+        } 
+        free(olds[i]);
+    }
+
+    free(olds);
+    return set;
+}
+
+
+void print_and_free(char** arr) {
+    char** p = arr;
+    while (*p != NULL) {
+        printf("%s\n", *p);
+        free(*p);
+        p++;
+    }
+    free(arr);
 }
 
 
@@ -50,13 +98,34 @@ int main() {
     char* ranks[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", NULL};
 
     char** items = product(suits, ranks);
+    print_and_free(items);
 
-    char** p = items;
-    while (*p != NULL) {
-        printf("%s\n", *p);
-        char** t = p;
-        free(*t);
-        p++;
+    char* counts[] = {"one", "two", "three", NULL};
+    char* shapes[]= {"diamond", "squiggle", "oval", NULL};
+    char* shadings[] = {"solid", "striped", "open", NULL};
+    char* colors[] = {"red", "green", "purple", NULL};
+
+    char*** olds = malloc(sizeof(char**) * 2);
+    char** set = product(counts, shapes);
+    olds[0] = set;
+    set = product(set, shadings);
+    olds[1] = set;
+    set = product(set, colors);
+
+    for (int i = 0; i < 2; i++) {
+        char** old_p = olds[i];
+        while (*old_p != NULL) {
+            free(*old_p);
+            old_p++;
+        } 
+        free(olds[i]);
     }
-    free(items);
+    print_and_free(set);
+    free(olds);
+
+    printf("\n\n\n\n");
+    char** arrs[] = {counts, shapes, shadings, colors, NULL};
+    char** m_set = multi_product(arrs);
+    print_and_free(m_set);
 }
+
